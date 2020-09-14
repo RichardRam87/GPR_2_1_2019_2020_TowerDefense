4f1497b11f9be78c196c809492445c68394adf26
Richard Ram <richard@richard-ram.com>
2020-09-14 11:59:05 +0200

    lelijke opzet van pathfollower

--- Assets/Scripts/PathFollower.cs
+++ Assets/Scripts/PathFollower.cs
@@ -1,21 +1,42 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PathFollower : MonoBehaviour
 {
-    [SerializeField] private Transform _target;
+    // Dit is niet DRY én niet dynamisch...
+    // kun je oplossen door een array te gebruiken
+    [SerializeField] private Transform _targetA;
+    [SerializeField] private Transform _targetB;
+    [SerializeField] private Transform _targetC;
+    
     [SerializeField] private float _speed;
     [SerializeField] private float _arrivalthreshold = 0.1f;
+
+    private Transform _currentWaypoint;
+
+    private void Start()
+    {
+        _currentWaypoint = _targetA;
+    }
+
     private void Update()
     {
-        Vector3 heightOffsetPosition = new Vector3(_target.position.x, transform.position.y, _target.position.z);
+        Vector3 heightOffsetPosition = new Vector3(_currentWaypoint.position.x, transform.position.y, _currentWaypoint.position.z);
         float distance = Vector3.Distance(transform.position, heightOffsetPosition);
         
         if (distance <= _arrivalthreshold)
         {
-            print("Ik ben er!");
+            if (_currentWaypoint == _targetA)
+            {
+                _currentWaypoint = _targetB;
+            }
+            else if (_currentWaypoint == _targetB)
+            {
+                _currentWaypoint = _targetC;
+            }
+            else if (_currentWaypoint == _targetC)
+            {
+                print("Ik ben bij het eindpunt!");
+            }
         }
         else
         {
