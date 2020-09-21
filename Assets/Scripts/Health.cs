151f70dee766b93fcf41a32fc278fbabcda7567d
Richard Ram <richard@richard-ram.com>
2020-09-21 12:24:52 +0200

    Implemented a UnityEvent

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float _startHealth = 3;
+
+    private float _currentHealth;
+
+    private void Start()
+    {
+        _currentHealth = _startHealth;
+    }
+
+    public void TakeDamage(float dmg)
+    {
+        _currentHealth -= dmg;
+        print("Ik krijg schade...");
+        
+        if (_currentHealth <= 0)
+        {
+            print("Ik ben dood");
+        }
+        
+    }
+}
--- Assets/Scripts/PathFollower.cs
+++ Assets/Scripts/PathFollower.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class PathFollower : MonoBehaviour
 {
@@ -6,6 +7,8 @@ public class PathFollower : MonoBehaviour
 
     [SerializeField] private float _speed;
     [SerializeField] private float _arrivalthreshold = 0.1f;
+
+    [SerializeField] private UnityEvent _OnPathComplete;
 
     private int _currentWaypointIndex;
 
@@ -16,15 +19,16 @@ public class PathFollower : MonoBehaviour
 
     private void Update()
     {
-        Vector3 heightOffsetPosition = new Vector3(_waypoints[_currentWaypointIndex].position.x, 
+        Vector3 heightOffsetPosition = new Vector3(_waypoints[_currentWaypointIndex].position.x,
             transform.position.y, _waypoints[_currentWaypointIndex].position.z);
         float distance = Vector3.Distance(transform.position, heightOffsetPosition);
 
         if (distance <= _arrivalthreshold)
         {
-            if (_currentWaypointIndex == _waypoints.Length-1)
+            if (_currentWaypointIndex == _waypoints.Length - 1)
             {
                 print("Ik ben bij het eindpunt");
+                _OnPathComplete?.Invoke();
             }
             else
             {
@@ -34,7 +38,7 @@ public class PathFollower : MonoBehaviour
         else
         {
             transform.LookAt(heightOffsetPosition);
-            transform.Translate(Vector3.forward * _speed * Time.deltaTime);    
+            transform.Translate(Vector3.forward * _speed * Time.deltaTime);
         }
     }
 }
