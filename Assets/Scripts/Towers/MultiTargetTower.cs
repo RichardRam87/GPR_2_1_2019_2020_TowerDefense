05553326bd00a6dc33ac434aa0a7f9811aa815bd
Richard Ram <richard@richard-ram.com>
2020-09-25 11:36:41 +0200

    opzet gemaakt van towers

---
+++ Assets/Scripts/Towers/MultiTargetTower.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiTargetTower : Tower<Enemy[]>
+{
+    protected override bool CanAttack()
+    {
+        _target = _rangeChecker.GetAllEnemiesInRange();
+        if (_target == null || _target.Length < 1) return false;
+
+        return true;
+    }
+
+    protected override void Attack()
+    {
+        foreach (Enemy enemy in _target)
+        {
+            print("multi target attack at: " + enemy.name);
+        }
+    }
+}
---
+++ Assets/Scripts/Towers/Tower.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(EnemyInRangeChecker))]
+public abstract class Tower<T> : MonoBehaviour
+{
+    protected EnemyInRangeChecker _rangeChecker;
+
+    protected T _target;
+
+    protected abstract bool CanAttack();
+    protected abstract void Attack();
+    private void Awake()
+    {
+        _rangeChecker = GetComponent<EnemyInRangeChecker>();
+    }
+
+    private void Update()
+    {
+        if (CanAttack())
+            Attack();
+    }
+}
