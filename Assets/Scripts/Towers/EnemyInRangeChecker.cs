289bbfdc15a60c363b2cf94957eba59ed90b0671
Richard Ram <richard@richard-ram.com>
2020-09-28 11:59:42 +0200

    basis opzet van tower class zonder inheritance

---
+++ Assets/Scripts/Towers/EnemyInRangeChecker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyInRangeChecker : MonoBehaviour
+{
+    [SerializeField] private float _radius;
+    [SerializeField] private LayerMask _layer;
+    public Enemy GetFirstEnemyInRange()
+    {
+        Collider[] cols = Physics.OverlapSphere(transform.position, _radius, _layer);
+        if (cols.Length < 1)
+            return null;
+
+        return cols[0].GetComponent<Enemy>();
+    }
+
+    public Enemy[] GetAllEnemiesInRange()
+    {
+        return null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, _radius);
+    }
+}
---
+++ Assets/Scripts/Towers/Tower.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    private EnemyInRangeChecker _rangeChecker;
+    private Enemy _target;
+    private void Awake()
+    {
+        _rangeChecker = GetComponent<EnemyInRangeChecker>();
+    }
+
+    void Update()
+    {
+        // als we niet kunnen aanvallen. Ga dan uit de update functie
+        if (!CanAttack()) return;
+        
+        Attack();
+    }
+    
+    private bool CanAttack()
+    {
+        _target = _rangeChecker.GetFirstEnemyInRange();
+        return _target != null;
+    }
+
+    private void Attack()
+    {
+        Debug.Log("Ik val aan!");
+    }
+}
