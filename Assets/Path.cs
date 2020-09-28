b3e7fca96e228f88c4de0ed096f7b3a931c97505
Richard Ram <richard@richard-ram.com>
2020-09-28 16:18:45 +0200

    basis path class

---
+++ Assets/Path.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    [SerializeField] private Waypoint[] _waypoints;
+
+    private void Awake()
+    {
+        _waypoints = GameObject.FindObjectsOfType<Waypoint>();
+        /*Array.Sort(_waypoints);
+        _waypoints.ToArray().so*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
